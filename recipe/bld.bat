0e106ab5b68676d918b206c1d12922ad2ce24d6d
Daniel Ching <dching@nvidia.com>
2026-09-18 21:26:06 -0500

    BLD: Accept any CUDA 13 major version

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -108,7 +108,7 @@ if not "%cuda_compiler_version%" == "None" (
 
     if "%cuda_compiler_version:~0,2%"=="12" (
         set "TORCH_CUDA_ARCH_LIST=5.0;6.0;7.0;7.5;8.0;8.6;9.0;10.0;12.0+PTX"
-    ) else if "%cuda_compiler_version%" == "13.0" (
+    ) else if "%cuda_compiler_version:~0,2%"=="13" (
         set "TORCH_CUDA_ARCH_LIST=7.5;8.0;8.6;9.0;10.0;11.0;12.0+PTX"
         REM c.f. https://github.com/pytorch/pytorch/pull/161316
         set "TORCH_NVCC_FLAGS=!TORCH_NVCC_FLAGS! -compress-mode=size"
